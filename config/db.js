75bfbc0abb3aa483970051771cafcd0680b9718b
HamidTech-Ventures <hamidtechventures@gmail.com>
2026-07-13 14:47:57 +0500

    Fix Mongoose connection for Serverless

--- config/db.js
+++ config/db.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose');
 
+let isConnected;
+
 const connectDB = async () => {
+  if (isConnected) {
+    console.log('Using existing MongoDB connection');
+    return;
+  }
+  
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
+    isConnected = conn.connections[0].readyState;
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    // Do not exit process in a serverless environment!
+    // process.exit(1);
+    throw error;
   }
 };
 
